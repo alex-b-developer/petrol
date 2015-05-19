6ed5d7bd55c76961915ab3fa254e0558639981f6
Foxy Themes <support@foxythemes.net>
2015-05-19 17:09:54 -0500

    Added: Js sidebar functionallity

--- src/assets/js/core.js
+++ src/assets/js/core.js
@@ -10,14 +10,39 @@ var App = (function () {
   };
 
   //Core private functions
+  function my_sidebar_func(){
 
+    $(".my-sidebar-elements-class > li > a").click(function( e ){//When we make click on main level item
+
+      var main_item = $(this).parent();
+      var sub_menu = $(this).next();//We get the sub menu UL element
+
+      //Part 1: We close all the open sub menus
+      $(".my-sidebar-elements-class > li.open").find(".sub-menu").slideUp(function(){
+        $(this).parent().removeClass("open");
+      });
+
+      //Part 2: We open the current clicked sub-menu 
+      sub_menu.slideDown(function(){//Execute when animation is completed
+        main_item.addClass("open");
+      });
+
+      e.preventDefault();//We stop the default href link action
+    });
+
+  }
+
+  var my_private_var = "Hell yeah";
+
+  //Public statements
   return {
     conf: config,
     init: function (options) {
       //Extends basic config with options
+
       $.extend( config, options );
 
-      //Core public function
+      my_sidebar_func( config );
 
     }
   };
