305f8531b13f4e49ec274e1f7cee6ae7bde345a4
Alejandro Mejía <diegomejiac@outlook.com>
2015-05-20 20:16:47 -0500

    Sidebar right icons css and jquery animation complete

--- src/assets/js/core.js
+++ src/assets/js/core.js
@@ -14,19 +14,69 @@ var App = (function () {
   function my_close_open_items(active_sub_menu){
 
     // Part 1: We close all the open sub menus
+
+    var icon_plus = $(".my-sidebar-elements-class > li > a > span.icon.icon-arrows_plus");
+    var icon_minus = $(".my-sidebar-elements-class > li > a > span.icon.icon-arrows_minus");
+    
+    console.log(icon_plus);
+    console.log(icon_minus);
+
     $(".my-sidebar-elements-class > li.open").find(".sub-menu").not(active_sub_menu).slideUp(function(){
       $(this).parent().removeClass("open");
-    });
 
+      icon_plus.css({
+        "opacity":"1",
+        "-webkit-transform":"rotate(0deg)",
+        "-moz-transform":"rotate(0deg)",
+        "-o-transform":"rotate(0deg)",
+        "-ms-transform":"rotate(0deg)",
+        "transform":"rotate(0deg)"
+      });
+
+      icon_minus.css({
+        "opacity":"0",
+        "-webkit-transform":"rotate(90deg)",
+        "-moz-transform":"rotate(90deg)",
+        "-o-transform":"rotate(90deg)",
+        "-ms-transform":"rotate(90deg)",
+        "transform":"rotate(90deg)"
+      });
+
+    });
   }
 
   function my_open_menu_item(sub_menu){
 
     // Part 2: We open the current clicked sub-menu 
     var main_item = sub_menu.parent();
+    var icon_plus = main_item.find("> a > span.icon.icon-arrows_plus");
+    var icon_minus = main_item.find("> a > span.icon.icon-arrows_minus");
 
     sub_menu.slideDown(function(){ //Execute when animation is completed
       main_item.addClass("open");
+
+      icon_plus.css({
+        "opacity":"0",
+        "-webkit-transform":"rotate(90deg)",
+        "-moz-transform":"rotate(90deg)",
+        "-o-transform":"rotate(90deg)",
+        "-ms-transform":"rotate(90deg)",
+        "transform":"rotate(90deg)"
+      });
+
+      icon_minus.css({
+        "opacity":"1",
+        "-webkit-transform":"rotate(0deg)",
+        "-moz-transform":"rotate(0deg)",
+        "-o-transform":"rotate(0deg)",
+        "-ms-transform":"rotate(0deg)",
+        "transform":"rotate(0deg)"
+      });
+
+
+   /* icon.fadeOut(100,function(){
+        $(this).removeClass("icon-arrows_plus").addClass("icon-arrows_minus").fadeIn(100);
+      }); */
     });
 
   }
