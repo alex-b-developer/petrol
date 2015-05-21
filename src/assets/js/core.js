af61e4e4fe196d9c6cf89cb892a6a1a04a807ece
Foxy Themes <support@foxythemes.net>
2015-05-21 18:53:15 -0500

    WIP: Sidebar icons animation order

--- src/assets/js/core.js
+++ src/assets/js/core.js
@@ -11,28 +11,29 @@ var App = (function () {
 
   // Core private functions
 
-  function my_close_open_items(active_sub_menu){
+  function my_close_open_items(sub_menu){
 
     // Part 1: We close all the open sub menus
+    var opened_submenus = $(".my-sidebar-elements-class > li.open").find(".sub-menu").not(sub_menu);
+    
+    var a = opened_submenus.prev();//Get the a element
+    var icon_plus = $("> span.icon.icon-arrows_plus", a);
+    var icon_minus = $("> span.icon.icon-arrows_minus", a);
+    
+    icon_plus.removeClass("rotate");
+    icon_minus.removeClass("rotate");
 
-    var icon_plus = $(".my-sidebar-elements-class > li > a > span.icon.icon-arrows_plus");
-    var icon_minus = $(".my-sidebar-elements-class > li > a > span.icon.icon-arrows_minus");
-
-    $(".my-sidebar-elements-class > li.open").find(".sub-menu").not(active_sub_menu).slideUp(function(){
+    opened_submenus.slideUp(function(){//After the animation ends
       $(this).parent().removeClass("open");
-
-      icon_plus.removeClass("rotate");
-      icon_minus.removeClass("rotate");
-
     });
   }
 
   function my_open_menu_item(sub_menu){
 
     // Part 2: We open the current clicked sub-menu 
-    var main_item = sub_menu.parent();
-    var icon_plus = main_item.find("> a > span.icon.icon-arrows_plus");
-    var icon_minus = main_item.find("> a > span.icon.icon-arrows_minus");
+    var main_item = sub_menu.parent();//Get the li element
+    var icon_plus = $("> a > span.icon.icon-arrows_plus", main_item);
+    var icon_minus = $("> a > span.icon.icon-arrows_minus", main_item);
 
     sub_menu.slideDown(function(){ //Execute when animation is completed
       main_item.addClass("open");
@@ -53,12 +54,12 @@ var App = (function () {
       var sub_menu = $(this).next(); //We get the sub menu UL element
 
       // Verify if the current item has a submenu
-      if(sub_menu.length > 0){
-        if(main_item.hasClass("open")){ // Don't open submenu if current main item is already closed
+      if( sub_menu.length > 0 ){
+        if( main_item.hasClass("open") ){ // Don't open submenu if current main item is already closed
           my_close_open_items();        
         }else{
-          my_close_open_items(sub_menu);
-          my_open_menu_item(sub_menu);
+          my_close_open_items( sub_menu );
+          my_open_menu_item( sub_menu );
         }
         e.preventDefault();//We stop the default href link action
       }
